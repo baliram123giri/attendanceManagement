bc78032479ca00d56d05347849d533352ab4f53b
baliram123giri <giri710401@gmail.com>
2023-12-07 00:09:21 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-export const baseUrl = "http://localhost:8000/api/v1"
-export const baseUrl1 = "https://api.bgtechub.com/api/v1"
+export const baseUrl1 = "http://localhost:8000/api/v1"
+export const baseUrl = "https://api.bgtechub.com/api/v1"
 
 export const getLocalStorageItem = (name) => {
     return JSON.parse(localStorage.getItem(name))
