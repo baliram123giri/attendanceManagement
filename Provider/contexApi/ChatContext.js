da403a4d4746da29b9c12554d735423b3403da6b
baliram123giri <giri710401@gmail.com>
2023-12-18 17:29:56 +0530

    Update ChatContext.js

--- Provider/contexApi/ChatContext.js
+++ Provider/contexApi/ChatContext.js
@@ -176,6 +176,7 @@ export const ChatContexProvider = ({ children }) => {
 
 
 
+
     return <ChatContex.Provider value={{ CreateChat, usersChat, updateChat: setCurrentChat, currentChat, sendMessage, messages,setMessages, onlineUsers, notifications, setNotifications }}>
         {children}
     </ChatContex.Provider>
