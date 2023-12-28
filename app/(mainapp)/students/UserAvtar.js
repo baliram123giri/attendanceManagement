cc21f306ccd62421ce93d6b7c0bdb3614695c600
baliram123giri <giri710401@gmail.com>
2023-12-28 10:41:15 +0530

    Update UserAvtar.js

--- app/(mainapp)/students/UserAvtar.js
+++ app/(mainapp)/students/UserAvtar.js
@@ -1,7 +1,6 @@
 "use client"
 import React from 'react'
 import Avatar from 'react-avatar'
-
 const UserAvtar = ({ name, avatar, color }) => {
     return (
         <Avatar size='30'  round {...(avatar ? { src: avatar } : { name, color })} />
